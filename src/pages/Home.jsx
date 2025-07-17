c63e9a21748b75b086612a5e6a9e7a02d95fbca7
amansaini97600 <amanks97600@gmail.com>
2025-07-17 22:02:08 +0530

    host website

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,17 @@ import CoursesSection from "./CoursesSection";
 import GallerySection from "./GallerySection";
 import FacultySection from "./FacultySection";
 import ContactSection from "./ContactSection";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>Home | CEC Computer Centre Dhampur</title>
+        <meta name="description" content="Enroll in computer courses at CEC Computer Centre Dhampur — the best place for C, C++, DCA, Python, Java, and more." />
+        <meta name="keywords" content="Coaching, Computer Courses, Dhampur, ADCA, DCA, CCC" />
+        <link rel="canonical" href="https://yourdomain.com/" />
+      </Helmet>
       <HeroSection />
       <AboutSection />
       <CoursesSection />
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: 'My Coaching Centre - Dhampur',
+          description: 'Best computer and typing coaching in Dhampur.',
+        },
+      },
+    }),
   ],
     server: {
     host: '0.0.0.0',
