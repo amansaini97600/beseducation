cb8310e50c2a3762451260c17d6ef8369617413c
aman saini <153929051+amansaini97600@users.noreply.github.com>
2025-07-20 19:52:05 +0530

    Update server.cjs

--- backend/server.cjs
+++ backend/server.cjs
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 
 // const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//const __dirname = path.dirname(__filename);
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.listen(PORT, () => {
